32151d6a7948d45822c7faaaddb8b7f29c2e8c27
sumitkumarsharm <sumitkumarsharma2748@gmail.com>
2025-10-30 11:55:57 +0530

    style: added company logo for our project

--- src/components/Student/Companies.jsx
+++ src/components/Student/Companies.jsx
@@ -4,7 +4,7 @@ import { assets } from '../../assets/assets'
 const Companies = () => {
   return (
     <div className='pt-6'>
-       <p className='text-gray-500'>trusted by learners and other Companies</p>
+       <p className='text-gray-500  ' >Trusted by <span className='font-semibold italic underline text-blue-500'>Learners</span> and other <span className='font-semibold text-blue-500 italic underline'>Companies</span> </p>
        <div className='flex flex-wrap items-center justify-center gap-6 md:gap-16 md:mt-10 mt-5'>
         <img src={assets.paypal_logo} alt="paypal_company_logo" className='w-20 md:w-28' />
         <img src={assets.accenture_logo} alt="paypal_company_logo" className='w-20 md:w-28' />
--- src/components/Student/CourseSection.jsx
+++ src/components/Student/CourseSection.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const CourseSection = () => {
   return (
-    <div>CourseSection</div>
-  )
-}
+    <div>
+      <h2> Master your craft with the best mentors.</h2>
+      <p>
+        Master your craft with the best mentors who guide you at every step,
+        helping you grow faster, learn smarter, and achieve lasting success.
+      </p>
+      <p></p>
+    </div>
+  );
+};
 
-export default CourseSection
+export default CourseSection;
